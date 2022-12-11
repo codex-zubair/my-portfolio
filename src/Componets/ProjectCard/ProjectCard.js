aa5c1da96b432f733f4db7fe6caeff81cf63ab21
zubair <codingwithrock@gmail.com>
2022-12-12 02:59:05 +0600

    daisy ui Theme setup

--- src/Componets/ProjectCard/ProjectCard.js
+++ src/Componets/ProjectCard/ProjectCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectCard = () => {
     return (
-        <div className='flex gap-5'>
+        <div className='flex gap-5 '>
             <div className='card bg-white'>
                 <h1>Category one</h1>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,29 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  themes: [
+    {
+      mytheme: {
+
+        "primary": "#4B6BFB",
+
+        "secondary": "#7B92B2",
+
+        "accent": "#67CBA0",
+
+        "neutral": "#FFFFFF",
+
+        "base-100": "#181A2A",
+
+        "info": "#3ABFF8",
+
+        "success": "#36D399",
+
+        "warning": "#FBBD23",
+
+        "error": "#F87272",
+      },
+    },
+  ],
   plugins: [require("daisyui")],
 }
