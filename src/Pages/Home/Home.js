afd9c19599e507931d1873c7907daa2dfeb5de2b
zubair <codingwithrock@gmail.com>
2022-12-12 11:54:17 +0600

    added download link by converted and dot env file setup

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import {  FaArrowDown} from 'react-icons/fa';
+import React, { useState } from 'react';
+import {  FaArrowDown , FaArrowAltCircleDown} from 'react-icons/fa';
 import Banner from '../../Componets/Banner/Banner';
 import ProjectCard from '../../Componets/ProjectCard/ProjectCard';
 
 const Home = () => {
+
+    // Show Project State 
+    const [display,setDisplay] = useState(false);
+
+
+
     return (
         <div className='flex flex-col items-center'>
            
@@ -11,10 +17,12 @@ const Home = () => {
 
 
             <div className='mt-5 flex flex-col items-center gap-5'>
-                <FaArrowDown className="text-white"></FaArrowDown>
-
+            <FaArrowDown className="animate-bounce text-white text-4xl"></FaArrowDown>
+                
                 <section className='flex gap-5'>
-                    <ProjectCard></ProjectCard>
+                    {display?<ProjectCard></ProjectCard> :<></> }
+                  
+
                 </section>
             </div>
         </div>
--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a href="https://doc-0o-b8-docs.googleusercontent.com/docs/securesc/td095a10g19k0qectd0illhvb7t0v6dn/s2a90i8k8t12n0aaba9f79k9ivsu8prs/1670822925000/01960886669908100593/01960886669908100593/1FZyx6TuDRE52kY7RN-7UOGwlWucpgFg_?e=download&ax=AEKYgyQMIioFtmygB9kQceMfcx6Q6ZONv2ZfCA5EGf7JKAzW5SuJXnZ1b1q8ehEdAFiixYHYNVZDs-4LBvwF3CWzHKMwIILw2XHnmCBsNJ5OZ4KNwVsb9_QYK0qK2uIOTubikfjWKGEqTyX_nh5VXvRQ9TFPc5N4fZI5KyuYZE9x4o99Vp6OFHIk8Wox2nVUHaZG0WpGzth6NYo6JjBz5v8LWZSQ4VlgOUI42RKlAtUpQuf0_n99eDTD3GNHuuXXEWBUEQuAt2vKjbfS9SX6O0DKScse3rzMaB5h2Esm2o1JkQ0fCVCl0MSE1OevM6LyQDrOMSarEsgRqBDlZgfFYF82bo_oX2O6j4juzUO1dlTSKvDGgpmXQkTUjYXoZMdgyjhefkQ5NYcDLeMIKW_Mhfdc3e5PbPi0aLNoFxSkqREA8ppQ4iFyLTQbNwc8Pshn0wPKzHEWucVF_x1nAIQ61BdX-LgwU74Yeb2rSYKFaDtXQc6luadg-bUAdkITYwU6LyE3DHGkJ5JHJj3BIdVyii9GwDvsT42ZxPfP5XmBweZMkPkSRGekXvXoLWyOq7Rm_z2Sq4UpzzHH1TkeYDiU8Kl2tnm0s1-nakxgc9VT4RXE696EIiEJgDa3R2zzORSx2dW5Uyh25qhLCbVpPsvJ-MO-3_Qw5i51r2ClEDEt3ihGYEWV1ie7ejAOlwFTQ97sZakMHLqevWStIhNoPVr-y9ifWs2xKB6r2e5YOQ3V6ZYpYOCGKOM9xV5seu1GZSdJykNDiNtqutepONV8mjTQvcpoD7tmuHjZ9YQNS6oUf6yXuCbrT9Br-rq7JdGoshnoTQjahe23lsPvhMQukugnoF6x2ww1HRrVVMrY6fzkQePrrk4xanXDaaI0Tv8gXXXjGZx2AHzCwJ0XQEAEGmwBP8ZFTo_TOXmnSUfYyTLeuO01IinB7OHY7jFyjeEmm9-L_pDkYE-JIiaOww&uuid=c9cc47e3-5bd6-4d9c-b489-c90dcd307248&authuser=2&nonce=rk2e5d082gugu&user=01960886669908100593&hash=v5dtm3j2jeh70c1j8nca0tqdajd16q7b" className="btn flex gap-2">Download Resume <FaDownload></FaDownload></a>
+                    <a href={`${process.env.REACT_APP_RESUME}`} className="btn flex gap-2">Download Resume <FaDownload></FaDownload></a>
 
 
 
