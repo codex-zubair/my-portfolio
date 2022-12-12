777dceb648d18f75a31f2707546fe346443df45b
zubair <codingwithrock@gmail.com>
2022-12-12 13:22:25 +0600

    added directly code link

--- src/Componets/ProjectCard/ProjectCard.js
+++ src/Componets/ProjectCard/ProjectCard.js
@@ -6,31 +6,33 @@ const ProjectCard = () => {
     return (
         <section className='lg:flex-row flex flex-col gap-2 w-11/12 mx-auto'>
 
-           
+
 
             <div className="card bg-base-100 shadow-xl image-full">
-                <figure><img src={spa}alt="Shoes" /></figure>
+                <figure><img src={spa} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title lg:text-3xl">Diana Spa</h2>
                     <p className='lg:text-xl'>Web Application For Spa Management</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-outline text-white">Project Details</button>
+                        <a href='https://github.com/codingwithrock/diana-spa-client'
+                            target="_blank" className="btn btn-outline text-white">Project Details</a>
                     </div>
                 </div>
             </div>
 
             <div className="card bg-base-100 shadow-xl image-full">
-                <figure><img src={ai}alt="Shoes" /></figure>
+                <figure><img src={ai} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title lg:text-3xl">AI Enthusiasts</h2>
                     <p className='lg:text-xl'>Learning Platform For AI Enthusiasts (Educational Website)</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-outline text-white">Project Details</button>
+                        <a href='https://github.com/codingwithrock/ai-client-side'
+                            target="_blank" className="btn btn-outline text-white">Project Details</a>
                     </div>
                 </div>
             </div>
 
-           
+
 
 
         </section>
