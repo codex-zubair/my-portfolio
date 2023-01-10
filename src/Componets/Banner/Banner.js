974a38e3a423a082bee99c55adb89154bc766f8f
zubair <codingwithrock@gmail.com>
2023-01-10 21:07:57 +0600

    fixed banner

--- src/Componets/Banner/Banner.js
+++ src/Componets/Banner/Banner.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { FaFacebook, FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import '../Banner/Banner.css';
 
 
 const Banner = () => {
     return (
         <section className='mx-2 lg:mx-20 my-10 lg:my-36 flex items-center flex-col text-white'>
-            <h1 className='text-2xl justify-center lg:text-5xl font-serif font-semibold'>I AM N.M. Zubair.</h1>
-            <p className='lg:text-xl my-6 text-center font-serif max-w-4xl'>I am a React developer. I am also proficient at backend development, but I enjoy working with React more on the front end of a project.
-                i have good understanding of error handling and fast acclimatization to new technology. Clean, efficient work is what I love to do.</p>
+            <h1 className='text-2xl justify-center lg:text-5xl font-serif font-semibold'>N.M. Zubair Alam.</h1>
+            <div>
+            <p className='lg:text-xl my-2 text-center font-serif max-w-4xl font-semibold typing-demo'>React Developer</p>
+            </div>
+            <p className='lg:text-xl my-6 text-center font-serif max-w-4xl font-semibold'>Hi! I am a React developer. I am also proficient at backend development, but I enjoy working with React more on the front end of a project.
+                i have good understanding of error handling and fast acclimatization to new technology. Clean, efficient work is what I love to do... 
+            <NavLink to={'/about'}>  more details</NavLink></p>
+
+            
 
 
             <div className='flex gap-5'>
@@ -17,6 +25,7 @@ const Banner = () => {
                 <a href='https://github.com/codingwithrock' target="_blank"><FaGithub className='text-3xl' /></a>
                 <a href='https://www.facebook.com/Zubayeralaam/' target="_blank"><FaFacebook className='text-3xl' /></a>
             </div>
+
         </section>
     );
 };
